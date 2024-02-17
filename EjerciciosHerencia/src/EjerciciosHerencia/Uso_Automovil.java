5f6df333643903185b636fdd2a7a76214a9acd58
luisc <luisc@LAPTOP-MEAG4UBS>
2024-02-17 18:56:04 +0100

    Ejercicio Herencia

--- EjerciciosHerencia/src/EjerciciosHerencia/Uso_Automovil.java
+++ EjerciciosHerencia/src/EjerciciosHerencia/Uso_Automovil.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
 public class Uso_Automovil {
 
 	public static void main(String[] args) {
-		Automovil automovil1 = new Automovil("Mercedes", "Clase C63", 2021, "Diesel");
-		JOptionPane.showMessageDialog(null,"Caracteristicas básicas\n" + automovil1 + "Tipo de combustible: " + automovil1.dimeTipoCombustible());
+		Automovil mercedes = new Automovil("Mercedes", "Clase C63", 2021, "Diesel");
+		JOptionPane.showMessageDialog(null,"Caracteristicas básicas\n" + mercedes + "Tipo de combustible: " + mercedes.dimeTipoCombustible());
 	}
 
 }
