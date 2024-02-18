cc0d3987d5e7a6cb7392bf5102e567b92cb0a5d5
luisc <luisc@LAPTOP-MEAG4UBS>
2024-02-18 21:18:15 +0100

    Animal(EJERCICIO 3)

--- EjerciciosABSTRACT/src/EjerciciosAnimal/Uso_Animal.java
+++ EjerciciosABSTRACT/src/EjerciciosAnimal/Uso_Animal.java
@@ -5,10 +5,10 @@ public class Uso_Animal {
 	public static void main(String[] args) {
 		perro Boby = new perro();
 		oveja Mariana = new oveja();
-		
+
 		Boby.sonido();
 		Boby.moverse();
-		
+
 		Mariana.sonido();
 		Mariana.moverse();
 
